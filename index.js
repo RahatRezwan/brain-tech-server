809ab27975b22a4120d935bb95d3516526482b32
Kazi Rahat <kazirahat1007@outlook.com>
2022-10-26 06:19:33 +0600

    create route for showing all courses api

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require("./course-data/courses-data.json");
+
 app.get("/", (req, res) => {
    res.send("Hello from brain tech server");
+});
+
+app.get("/courses", (req, res) => {
+   res.send(courses);
 });
 
 app.listen(port, () => {
