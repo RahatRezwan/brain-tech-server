d8f26b8d1316b517c891ae7f2501a23979528b23
Kazi Rahat <kazirahat1007@outlook.com>
2022-10-26 06:36:42 +0600

    create a route to display a sigle course by id

--- index.js
+++ index.js
@@ -8,18 +8,26 @@ app.use(cors());
 const courses = require("./course-data/courses-data.json");
 const categories = require("./course-data/category.json");
 
+/* Home Route */
 app.get("/", (req, res) => {
    res.send("Hello from brain tech server");
 });
 
-/* route to display all categories */
+/* Route to display all categories */
 app.get("/categories", (req, res) => {
    res.send(categories);
 });
 
-/* route to display all courses data */
+/* Route to display all courses data */
 app.get("/courses", (req, res) => {
    res.send(courses);
+});
+
+/* Route to display a single course details */
+app.get("/course/:courseId", (req, res) => {
+   const courseId = req.params.courseId;
+   const currentCourse = courses.find((c) => c.id === courseId);
+   res.send(currentCourse);
 });
 
 app.listen(port, () => {
